d0cfea7742347c250c906cdfc783d054501504ad
rajanjamu <rajanjamu@gmail.com>
2021-05-07 20:53:44 +0530

    Styling of lists and list item

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -5,10 +5,23 @@ import selectExpenses from '../selectors/expenses';
 
 const ExpenseList = (props) => (
   <div className="content-container">
-    <h1>Expense List</h1>
-    {props.expenses.map((expense) => (
-      <ExpenseListItem key={expense.id} {...expense} />
-    ))}
+    <div className="list-header">
+      <div className="show-for-mobile">Expenses</div>
+      <div className="show-for-desktop">Expense</div>
+      <div className="show-for-desktop">Amount</div>
+    </div>
+
+    <div className="list-body">
+      {props.expenses.length === 0 ? (
+        <div className="list-item list-item--message">
+          <span>No expenses</span>
+        </div>
+      ) : (
+        props.expenses.map((expense) => (
+          <ExpenseListItem key={expense.id} {...expense} />
+        ))
+      )}
+    </div>
   </div>
 );
 
